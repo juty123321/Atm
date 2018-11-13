c71c90025cff9bb8589fec1676addd54a513787a
user <juty123321@gmail.com>
2018-11-13 16:38:39 +0800

    2018.11.13

--- app/src/main/java/com/example/bunnychiu/atm/LoginActivity.java
+++ app/src/main/java/com/example/bunnychiu/atm/LoginActivity.java
@@ -5,22 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText ed_username;
+    private EditText ed_password;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText edusername = findViewById(R.id.ed_username);
+        String username = getSharedPreferences("atm",MODE_PRIVATE)
+                .getString("USERNAME","");
+        edusername.setText(username);
     }
 
     public void login(View view) {
-        String userid = ((EditText)findViewById(R.id.ed_userid)).getText().toString();
-        String passwd = ((EditText)findViewById(R.id.ed_passwd)).getText().toString();
-        if ("juty123321".equals(userid) && "123456789".equals(passwd)) {
+        ed_username = findViewById(R.id.ed_username);
+        ed_password = findViewById(R.id.ed_password);
+        username = ed_username.getText().toString();
+        password = ed_password.getText().toString();
+        if ("juty123321".equals(username) && "123456789".equals(password)) {
             getSharedPreferences("atm",MODE_PRIVATE)
-                    .edit().putString("USERID",userid).apply();
+                    .edit().putString("USERID",username).apply();
             finish();
         }else{ //登入失敗
             new AlertDialog.Builder(this)
